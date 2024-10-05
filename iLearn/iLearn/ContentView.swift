049a2892d9e96beefb98180f7cfffbd8bf393e2d
aiexxash <ialexballa@gmail.com>
2024-10-05 21:35:54 +0300

    Create alert on toolbar button click

--- iLearn/iLearn/ContentView.swift
+++ iLearn/iLearn/ContentView.swift
@@ -13,7 +13,9 @@ struct Theme: Identifiable {  // Create a "Theme" structure as an element list
 }
 
 struct ContentView: View {
-    private var themes = [  // Set values for elements using "Theme" structure
+    @State private var showCreateAlert: Bool = false
+    @State private var titleThemeTextFieldText: String = ""
+    @State private var themes = [  // Set values for elements using "Theme" structure
         Theme(name: "Deutsch 1"),
         Theme(name: "GER Lesson 1 HW"),
         Theme(name: "English advanced C1")
@@ -34,9 +36,24 @@ struct ContentView: View {
             .navigationBarTitleDisplayMode(.large)
             .toolbar { // Interact with toolbar
                 Button(action: { // Add right toolbar plus button
-                    print("add tapped")
+                    showCreateAlert = true // Show the alert ob button tap
                 }) {
                     Image(systemName: "plus")
+                }.alert(
+                    Text("Add new theme"), // Title for the alert
+                    isPresented: $showCreateAlert // Switch on a variable
+                ) {
+                    Button("Cancel", role: .cancel) {} // Cancel button
+                    Button("Create") {
+                        if !titleThemeTextFieldText.isEmpty { // If the title is not equal nil ('!' at the begining) do the function
+                            themes.append(Theme(name: titleThemeTextFieldText))
+                            showCreateAlert = false // Hide the alert
+                        }
+                    }
+                    
+                    TextField("Title", text: $titleThemeTextFieldText) // Create a textField
+                } message: {
+                    Text("Create your new learning journey") // Alert message
                 }
             }
         }
