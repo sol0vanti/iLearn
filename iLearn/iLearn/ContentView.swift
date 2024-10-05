0309a61b52b8262ed6c6d3be156fef6978174528
aiexxash <ialexballa@gmail.com>
2024-10-05 12:42:09 +0300

    Code comments

--- iLearn/iLearn/ContentView.swift
+++ iLearn/iLearn/ContentView.swift
@@ -7,13 +7,13 @@
 
 import SwiftUI
 
-struct Theme: Identifiable {
+struct Theme: Identifiable {  // Create a "Theme" structure as an element list
     let name: String
     let id = UUID()
 }
 
 struct ContentView: View {
-    private var themes = [
+    private var themes = [  // Set values for elements using "Theme" structure
         Theme(name: "Deutsch 1"),
         Theme(name: "GER Lesson 1 HW"),
         Theme(name: "English advanced C1")
@@ -22,18 +22,18 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(themes) { theme in
-                    NavigationLink {
+                ForEach(themes) { theme in // Loop all theme in themes list
+                    NavigationLink { // Go to the "PlayModeView" on element click
                         PlayModeView(selectedTheme: theme.name)
                     } label: {
-                        Text(theme.name)
+                        Text(theme.name) // Create a title for List element
                     }
                 }
             }
             .navigationTitle("All Themes")
             .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                Button(action: {
+            .toolbar { // Interact with toolbar
+                Button(action: { // Add right toolbar plus button
                     print("add tapped")
                 }) {
                     Image(systemName: "plus")
